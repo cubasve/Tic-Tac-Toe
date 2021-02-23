8fc0677aba5fd7a265fad77dfdce5b283bc27d9b
cubasve <eva.cubasvasquez@gmail.com>
2021-02-23 00:01:33 -0500

    Add constants, variables, cached element references to game

---
+++ main.js
@@ -0,0 +1,46 @@
+/*------CONSTANTS-------- */
+const colors = {
+    '1': 'green', //player 1 is green
+    '-1': 'blue', //player 2 is blue
+    'null': 'white', //when square is empty
+}
+
+const winningCombinations = [
+    //nested array - contains 3 indices of the board
+    [0, 1, 2], //1st row
+    [3, 4, 5], //2nd row
+    [6, 7, 8], //3rd row
+    [0, 3, 6], //1st column
+    [1, 4, 7], //2nd column
+    [2, 5, 8], //3rd column
+    [0, 4, 8], //diagonal: top left to bottom right
+    [2, 4, 6], //diagonal: top right to bottom left
+]
+
+/*------APP'S STATE (VARIABLES)-------- */
+//NOTE: All are just declared, not assigned yet
+let board, turn, winner;
+
+/*------CACHED ELEMENT REFERENCES-------- */
+const squares = document.querySelectorAll('td div');
+const message = document.querySelector('h1');
+
+/*------EVENT LISTENERS-------- */
+document.querySelector('table').addEventListener('click', handleMove);
+document.querySelector('button').addEventListener('click', initialize);
+
+/*------FUNCTIONS-------- */
+
+initialize();
+
+function handleMove() {
+    
+}
+
+function render() {
+
+}
+
+function initialize() {
+
+}
